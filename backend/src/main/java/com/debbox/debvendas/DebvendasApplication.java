6ddf0e8ecccf33a35aadb679201ea71c8313e836
Caio Pinto <83309444+TheHiyoko@users.noreply.github.com>
2021-11-04 22:10:33 -0300

    primeiro commit

---
+++ backend/src/main/java/com/debbox/debvendas/DebvendasApplication.java
@@ -0,0 +1,13 @@
+package com.debbox.debvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/debbox/debvendas/DebvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debbox.debvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
