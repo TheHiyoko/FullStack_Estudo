076af2308b5102c313e17723c3ab1ecbf54c4230
Caio Pinto <83309444+TheHiyoko@users.noreply.github.com>
2021-11-05 17:33:30 -0300

    static done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/barChart/barChart";
 import DataTable from "components/dataTable/dataTable";
+import DonutChart from "components/DounatChart/DonutChart";
 import Footer from "components/footer/footer";
 import NavBar from "components/NavBar/NavBar";
 
@@ -7,8 +9,22 @@ function App() {
     <>
       <NavBar/>
       <div className="container">
-        <h1 className="text-primary">Olá mundo</h1>
+        <h1 className="text-primary py-3">Dashbord de vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h4 className="text-primary text-center">Todas as vendas</h4>
+            <BarChart/>
+          </div>
+          <div className="col-sm-6">
+            <h4 className="text-primary text-center">Todas as vendas</h4>
+            <DonutChart/>
+          </div>
+        </div>
+      <div>
+        <h2 className="text-primatry py-3">Todas as vendas</h2>
         <DataTable/>
+      </div>
       </div>
       <Footer/>
     </>
