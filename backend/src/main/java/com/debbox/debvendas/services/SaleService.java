01e41d4a3f161488fef12b226ff06f71d16b3662
Caio Pinto <83309444+TheHiyoko@users.noreply.github.com>
2021-11-30 14:58:37 -0300

    pagination

--- backend/src/main/java/com/debbox/debvendas/services/SaleService.java
+++ backend/src/main/java/com/debbox/debvendas/services/SaleService.java
@@ -4,11 +4,13 @@ package com.debbox.debvendas.services;
 import com.debbox.debvendas.dto.SaleDTO;
 import com.debbox.debvendas.entities.Sale;
 import com.debbox.debvendas.repositories.SaleRepository;
+import com.debbox.debvendas.repositories.SellerRepositorie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SaleService {
@@ -16,7 +18,12 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
+    @Autowired
+    private SellerRepositorie sellerRepositoie;
+
+    @Transactional(readOnly = true)
     public Page<SaleDTO> findAll(Pageable pageable){
+        sellerRepositoie.findAll();
         Page<Sale> result = repository.findAll(pageable);
         return result.map(x -> new SaleDTO(x));
 
